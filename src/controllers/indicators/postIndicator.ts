77c9b26bb5db4ae196efcc62f1cf429175b09615
one92tb <one92tb@gmail.com>
2020-05-31 00:41:05 +0200

    change indicator entity (add column with isDefault), hide and change user name and password in ormconfig

--- src/controllers/indicators/postIndicator.ts
+++ src/controllers/indicators/postIndicator.ts
@@ -3,6 +3,7 @@ import { getManager } from "typeorm";
 import { Indicator } from "../../entity/Indicator";
 
 export async function postIndicator(request: Request, response: Response) {
+  console.log(request.body);
   const indicatorRepository = getManager().getRepository(Indicator);
   const indicator = {
     name: request.body.name,
@@ -12,6 +13,7 @@ export async function postIndicator(request: Request, response: Response) {
     street: request.body.street,
     city: request.body.city,
     country: request.body.country,
+    isDefault: request.body.isDefault,
     userId: request.user.userData.userId
   };
 
--- src/entity/Indicator.ts
+++ src/entity/Indicator.ts
@@ -28,6 +28,9 @@ export class Indicator {
     @Column()
     country: string;
 
+    @Column()
+    isDefault: boolean;
+
     @Column({ type: "int" })
     public userId: number;
 
