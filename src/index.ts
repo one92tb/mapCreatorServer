7c3bfcc21ed409830ab2223b6acf8a16ffe7be4e
one92tb <one92tb@gmail.com>
2020-06-22 22:25:56 +0200

    problem with dotenv

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { createConnection } from "typeorm";
 import { ConnectionOptions } from "typeorm";
 import { getManager, getRepository } from "typeorm";
 import { Marker } from "./entity/Marker";
+import * as dotenv from 'dotenv';
+
 const express = require("express");
 const app = express();
 const multer = require("multer");
@@ -15,6 +17,12 @@ const flash = require("connect-flash");
 const jwt = require("express-jwt");
 
 import { routes } from "./routes/index";
+
+dotenv.config();
+const userName = process.env.USER_NAME;
+console.log(userName);
+let env = process.env["NODE_ENV"];
+console.log(env);
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 8080;
@@ -48,14 +56,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-//const args = (route.method === 'post' && route.path === '/markers') ? (route.path, upload.any(), (request: Request, response: Response, next: Function) : (route.path, (request: Request, response: Response, next: Function);
 
 const options: ConnectionOptions = {
   type: "sqlite",
   logging: true,
   database: "db/mydb.db",
   synchronize: true,
-  entities: ['build/entity/**/*.js'],
+  entities: ['src/entity/**/*.ts'],
 };
   //entities: ['build/entity/**/*.js'],
 createConnection(options).then(async connection => {
